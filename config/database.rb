c1c8d6db55fadedc68233a022a1bded3f33b48bd
Efrén Fuentes <efrenfuentes@gmail.com>
2016-01-27 22:07:39 -0430

    Adjust Mongoid logs

--- config/database.rb
+++ config/database.rb
@@ -3,6 +3,8 @@ require 'mongoid'
 
 Mongoid.load!(APP_ROOT.join('config', 'mongoid.yml'), Sinatra::Base.environment.to_sym)
 Mongoid.raise_not_found_error = false
+Mongoid.logger.level = Logger::INFO
+Mongo::Logger.logger.level = Logger::INFO
 
 # Set up the models
 Dir[APP_ROOT.join('app', 'models', '*.rb')].each { |file| require file }
